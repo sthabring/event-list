215670df291a6d0d61f937f7a204d58a34c6362e
Stefan Habring <stefan.habring@elements.at>
2020-12-04 09:37:28 +0100

    Setup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import eventData from './features/events/event-data.json';
+
+console.log(eventData);
 
 function App() {
-  return (
-    <div className="container mt-5">
-      <h2>Events</h2>
+    return (
+        <div className="container mt-5">
+            <h2>Events</h2>
 
-        <div className="alert alert-warning">
-            Todo
+            <div className="alert alert-warning">
+                Todo: create event teaser
+            </div>
+
+            <pre><code>{JSON.stringify(eventData, null, 2)}</code></pre>
         </div>
-    </div>
-  );
+    );
 }
 
 export default App;
--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
-import eventReducer from '../features/events/events-reducer';
 
 export default configureStore({
   reducer: {
-    events: eventReducer,
   },
 });
